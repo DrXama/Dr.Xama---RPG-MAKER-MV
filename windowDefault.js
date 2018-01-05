6d04a0de9181809694317ad1423186f4baff4424
Dr.Xamã <SPGamesOficial@gmail.com>
2018-01-05 06:48:13 -0200

    Lançado
    - Usado para criar as janelas

---
+++ windowDefault.js
@@ -0,0 +1,31 @@
+//-----------------------------------------------------------------------------
+// Window_Default
+//
+
+function Window_Default() {
+    this.initialize.apply(this, arguments);
+}
+
+Window_Default.prototype = Object.create(Window_Base.prototype);
+Window_Default.prototype.constructor = Window_Default;
+
+Window_Default.prototype.initialize = function (x, y) {
+    var width = this.windowWidth();
+    var height = this.windowHeight();
+    Window_Base.prototype.initialize.call(this, x, y, width, height);
+    this.refresh();
+};
+
+Window_Default.prototype.windowWidth = function () {
+    return 240;
+};
+
+Window_Default.prototype.windowHeight = function () {
+    return this.fittingHeight(1);
+};
+
+Window_Default.prototype.refresh = function () {
+    var x = this.textPadding();
+    var width = this.contents.width - this.textPadding() * 2;
+    this.contents.clear();
+};

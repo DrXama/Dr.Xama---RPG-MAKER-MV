fec1c536fe2b7360e542f028539629d3002f742b
Dr.Xamã <SPGamesOficial@gmail.com>
2018-05-23 13:07:28 -0300

    novos meios de atualizar

--- pluginDefault.js
+++ pluginDefault.js
@@ -14,8 +14,9 @@
  * ================================================================================
  *    Atualização
  * ================================================================================
- * Para atualizar esse plugin vá no github do Dr.Xamã
- * https://github.com/GS-GAME-WORDS/Dr.Xama---RPG-MAKER-MV
+ * Para atualizar esse plugin vá no site/github do Dr.Xamã!
+ * - Site: http://drxama.epizy.com/?page_id=299
+ * - Github: https://github.com/GS-GAME-WORDS/Dr.Xama---RPG-MAKER-MV
  */
 (function () {
     "use strict";

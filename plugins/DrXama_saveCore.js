073b1993166cf903e73d110d97d275067444c21c
GuilhermeSantos001 <luizgp120@hotmail.com>
2019-09-05 22:16:53 -0300

    - Armazenamento de itens completo!

--- plugins/DrXama_saveCore.js
+++ plugins/DrXama_saveCore.js
@@ -13,6 +13,91 @@
  * Sistema para salvar dinheiro, itens, poções, membros do grupo e etc.
  * Eficiente para armazenamento de informações.
  * ================================================================================
+ *    Leia-Me!
+ * ================================================================================
+ * O plugin armazena de forma individual em um save exclusivo, dessa forma você
+ * coleta informações em qualquer save do seu jogo, e isso deixa o jogo dinamico,
+ * você pode armazenar informações de diferentes saves e usar em qualquer um deles.
+ * ================================================================================
+ *    Comandos de Plugin
+ * ================================================================================
+ * --------------------------------------------------------------------------------
+ * Para armazenar o ouro na memoria.
+ * --------------------------------------------------------------------------------
+ * store gold identifier amount
+ * 
+ * - identifier : Um ID para separar a informação de forma organizada na data.
+ *                Lembre-se que o sistema salva o identifier da forma como você
+ *                escreveu ou seja, se você digitar: 'Inventario1' e for usar
+ *                depois buscando por 'inventario1' não vai dar certo, escreva
+ *                da mesma forma para funcionar.
+ * 
+ * - amount : A quantidade a ser armazenada. Caso a quantidade seja maior
+ *            do que o jogador possui, ou seja, você coloca 50 na quantidade
+ *            e o jogador tem 5, logo a formula do calculo vai fazer o 50
+ *            virar 5, que é o maximo.
+ * 
+ * Exemplo:
+ * store gold MyInventory 652
+ * --------------------------------------------------------------------------------
+ * Para recuperar o ouro armazenado.
+ * --------------------------------------------------------------------------------
+ * recover gold identifier amount loseAmount
+ * 
+ * - identifier : Um ID para separar a informação de forma organizada na data.
+ *                Lembre-se que o sistema salva o identifier da forma como você
+ *                escreveu ou seja, se você digitar: 'Inventario1' e for usar
+ *                depois buscando por 'inventario1' não vai dar certo, escreva
+ *                da mesma forma para funcionar.
+ * 
+ * - amount : A quantidade a ser recuperada. Caso a quantidade seja maior
+ *            do que a armazenada, ou seja, você coloca 50 na quantidade
+ *            e o valor armazenado é 5, logo a formula do calculo vai fazer o 50
+ *            virar 5, que é o maximo.
+ * 
+ * - loseAmount : Coloque 'false' caso você não queira que o valor armazenado
+ *                seja diminuido. Use 'true' para diminuir o valor armazenado.
+ * --------------------------------------------------------------------------------
+ * Para armazenar um item na memoria.
+ * --------------------------------------------------------------------------------
+ * store item identifier itemID amount
+ * 
+ * - identifier : Um ID para separar a informação de forma organizada na data.
+ *                Lembre-se que o sistema salva o identifier da forma como você
+ *                escreveu ou seja, se você digitar: 'Inventario1' e for usar
+ *                depois buscando por 'inventario1' não vai dar certo, escreva
+ *                da mesma forma para funcionar.
+ * 
+ * - itemID : ID do item, esse valor é o mesmo do banco de dados do sistema.
+ * 
+ * - amount : A quantidade a ser armazenada. Caso a quantidade seja maior
+ *            do que o jogador possui, ou seja, você coloca 50 na quantidade
+ *            e o jogador tem 5, logo a formula do calculo vai fazer o 50
+ *            virar 5, que é o maximo.
+ * 
+ * Exemplo:
+ * store item MyInventory 1 2 false
+ * --------------------------------------------------------------------------------
+ * Para recuperar o item armazenado.
+ * --------------------------------------------------------------------------------
+ * recover item identifier itemId amount loseAmount
+ * 
+ * - identifier : Um ID para separar a informação de forma organizada na data.
+ *                Lembre-se que o sistema salva o identifier da forma como você
+ *                escreveu ou seja, se você digitar: 'Inventario1' e for usar
+ *                depois buscando por 'inventario1' não vai dar certo, escreva
+ *                da mesma forma para funcionar.
+ * 
+ * - itemID : ID do item, esse valor é o mesmo que você usou para armazenar-lo.
+ * 
+ * - amount : A quantidade a ser recuperada. Caso a quantidade seja maior
+ *            do que a armazenada, ou seja, você coloca 50 na quantidade
+ *            e o valor armazenado é 5, logo a formula do calculo vai fazer o 50
+ *            virar 5, que é o maximo.
+ * 
+ * - loseAmount : Coloque 'false' caso você não queira que o valor armazenado
+ *                seja diminuido. Use 'true' para diminuir o valor armazenado.
+ * ================================================================================
  *    Atualização
  * ================================================================================
  * Para atualizar esse plugin vá no Github do Dr.Xamã!
@@ -78,7 +163,7 @@ DX.pluginName = DX.pluginName || {
     };
 
     function typeofIsBad(obj) {
-        return typeof obj === null || typeof obj === undefined;
+        return obj === null || obj === undefined;
     };
 
     //-----------------------------------------------------------------------------
@@ -108,19 +193,48 @@ DX.pluginName = DX.pluginName || {
 
     Game_Temp.prototype.storeGold = function (identifier, gold) {
         if (typeofIsBad(identifier) || typeofIsBad(gold)) return;
-        if ($gameParty.gold() - gold < 0)
-            gold = gold - (gold - $gameParty.gold());
+        if (typeofIsBad(this._saveCore[identifier])) this._saveCore[identifier] = {};
+        if ($gameParty.gold() - gold < 0) gold = gold - (gold - $gameParty.gold());
         $gameParty.loseGold(gold);
-        this._saveCore[identifier] = gold;
+        this._saveCore[identifier].gold = gold;
         this.storeSaveCore();
     };
 
-    Game_Temp.prototype.recoverGold = function (identifier, gold) {
+    Game_Temp.prototype.recoverGold = function (identifier, gold, loseGold) {
         if (typeofIsBad(identifier) || typeofIsBad(gold)) return;
-        if (this._saveCore[identifier] - gold < 0)
-            gold = gold - (gold - this._saveCore[identifier]);
+        if (typeofIsBad(this._saveCore[identifier])) return;
+        if (this._saveCore[identifier].gold - gold < 0)
+            gold = gold - (gold - this._saveCore[identifier].gold);
         $gameParty.gainGold(gold);
-        this._saveCore[identifier] -= gold;
+        if (loseGold) this._saveCore[identifier].gold -= gold;
+        this.storeSaveCore();
+    };
+
+    Game_Temp.prototype.storeItem = function (identifier, itemId, amount) {
+        if (typeofIsBad(identifier) || typeofIsBad(itemId) || typeofIsBad(amount)) return;
+        if (typeofIsBad(this._saveCore[identifier])) this._saveCore[identifier] = {};
+        if (typeofIsBad(this._saveCore[identifier].items)) this._saveCore[identifier].items = {};
+        let item = $gameParty.items().find(item => { return item.id === itemId; });
+        if (typeofIsBad(item)) return;
+        if ($gameParty.numItems(item) <= 0) return;
+        if ($gameParty.numItems(item) - amount < 0)
+            amount = amount - (amount - $gameParty.numItems(item));
+        this._saveCore[identifier].items[itemId] = amount;
+        $gameParty.loseItem(item, amount);
+        this.storeSaveCore();
+    };
+
+    Game_Temp.prototype.recoverItem = function (identifier, itemId, amount, loseAmount) {
+        if (typeofIsBad(identifier) || typeofIsBad(itemId) || typeofIsBad(amount)) return;
+        if (typeofIsBad(this._saveCore[identifier]) || typeofIsBad(this._saveCore[identifier].items)) return;
+        let item = $dataItems.find(item => { if (!typeofIsBad(item)) return item.id === itemId; });
+        if (typeofIsBad(item)) return;
+        if (typeofIsBad(this._saveCore[identifier].items[itemId])) return;
+        if (this._saveCore[identifier].items[itemId] <= 0) return;
+        if (this._saveCore[identifier].items[itemId] - amount < 0)
+            amount = amount - (amount - this._saveCore[identifier].items[itemId]);
+        if (loseAmount) this._saveCore[identifier].items[itemId] -= amount;
+        $gameParty.gainItem(item, amount);
         this.storeSaveCore();
     };
 
@@ -135,10 +249,16 @@ DX.pluginName = DX.pluginName || {
             if (String(args[0]).toLocaleLowerCase() === 'gold') {
                 $gameTemp.storeGold(String(args[1]), Number(args[2]));
             }
+            if (String(args[0]).toLocaleLowerCase() === 'item') {
+                $gameTemp.storeItem(String(args[1]), Number(args[2]), Number(args[3]));
+            }
         }
         if (command === 'recover') {
             if (String(args[0]).toLocaleLowerCase() === 'gold') {
-                $gameTemp.recoverGold(String(args[1]), Number(args[2]));
+                $gameTemp.recoverGold(String(args[1]), Number(args[2]), Boolean(args[3]));
+            }
+            if (String(args[0]).toLocaleLowerCase() === 'item') {
+                $gameTemp.recoverItem(String(args[1]), Number(args[2]), Number(args[3]), Boolean(args[4]));
             }
         }
     };
